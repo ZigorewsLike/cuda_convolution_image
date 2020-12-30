08c9f0bb09cad513eb754d866d16de529c18926a
Cherepanov Igor <zigorewslike@gmail.com>
2020-12-30 17:16:24 +0800

    Добавление UI и коррекция ядра

--- kernel_to_dll/kernel.cu
+++ kernel_to_dll/kernel.cu
@@ -10,7 +10,13 @@
 using namespace std;
 using namespace std::chrono;
 
-__global__ void add_vector(int* img, int* conv, int* c, int N, int M, int cN)
+void handleCudaError(cudaError_t cudaERR) {
+    if (cudaERR != cudaSuccess) {
+        printf("CUDA ERROR : %s\n", cudaGetErrorString(cudaERR));
+    }
+}
+
+__global__ void add_vector(unsigned int* img, int* conv, unsigned int* c, int N, int M, int cN)
 {
     long tid_x = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid_x < N * M) {
@@ -28,30 +34,32 @@ __global__ void add_vector(int* img, int* conv, int* c, int N, int M, int cN)
                 sum += conv[i] * img[thr_x];
             }
             //if (i == 1)
-                //printf("imgx = %i, tid_x = %i, imgy = %i\n x = %i, tid_x = %i, new_tid_x = %i, y = %i\n --\n", img_x, tid_x, img_y, x, tid_x, thr_x, y);
+            //    printf("imgx = %i, tid_x = %i, imgy = %i\n x = %i, tid_x = %i, new_tid_x = %i, y = %i\n Sum = %i img = %i conv = %i\n--\n", img_x, tid_x, img_y, x, tid_x, thr_x, y, sum, img[thr_x], conv[i]);
         }
-        sum /= d;
+        if (d != 0) sum /= abs(d);
+        if (sum < 0) sum = 0;
+        if (sum > 255) sum = 255;
+        //printf("tid_x = %i Val = %i\n --\n", tid_x, sum);
         c[tid_x] = sum;
     }
 }
 
-DELLEXPORT int* calcConvolutionCuda(int N, int M, int* img, int* conv, int cN) {
+DELLEXPORT unsigned int* calcConvolutionCuda(int N, int M, unsigned int* img, int* conv, int cN) {
     int cuda_count;
     cudaGetDeviceCount(&cuda_count);
-    //printf("Cuda device count = %i\n", cuda_count);
+    printf("Cuda device count = %i\n", cuda_count);
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     float g_time = 0.0;
 
-    int* c = new int[N * M];
+    unsigned int* c = new unsigned int[N * M];
 
     if (cuda_count == 0) {
         cout << "Cuda device not found!";
         return NULL;
     }
-
     cudaDeviceProp info;
     cudaGetDeviceProperties(&info, 0);
     unsigned int maxCudaTreads = info.maxThreadsPerBlock;
@@ -60,18 +68,18 @@ DELLEXPORT int* calcConvolutionCuda(int N, int M, int* img, int* conv, int cN) {
     }
     unsigned int maxCudaBlocks = (N * M + maxCudaTreads - 1) / maxCudaTreads;
 
-    //printf("threads: %i blocks: %i\n", maxCudaTreads, maxCudaBlocks);
+    printf("N = %i M = %i\n threads: %i blocks: %i\n", N, M, maxCudaTreads, maxCudaBlocks);
 
-    int* dev_a;
+    unsigned int* dev_a;
     int* dev_b;
-    int* dev_c;
+    unsigned int* dev_c;
 
-    cudaMalloc((void**)&dev_a, N * M * sizeof(int));
+    cudaMalloc((void**)&dev_a, N * M * sizeof(unsigned int));
     cudaMalloc((void**)&dev_b, N * M * sizeof(int));
-    cudaMalloc((void**)&dev_c, N * M * sizeof(int));
+    cudaMalloc((void**)&dev_c, N * M * sizeof(unsigned int));
 
-    cudaMemcpy(dev_a, img, N * M * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, conv, N * M * sizeof(int), cudaMemcpyHostToDevice);
+    handleCudaError(cudaMemcpy(dev_a, img, N * M * sizeof(unsigned int), cudaMemcpyHostToDevice));
+    handleCudaError(cudaMemcpy(dev_b, conv, N * M * sizeof(int), cudaMemcpyHostToDevice));
 
     cudaEventRecord(start, 0);
 
@@ -83,13 +91,13 @@ DELLEXPORT int* calcConvolutionCuda(int N, int M, int* img, int* conv, int cN) {
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
 
-    cudaMemcpy(c, dev_c, N * M * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, dev_c, N * M * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
 
-    //printf(" time (gpu)= %f mm.\n Calc %i elem\n", g_time, N * M);
+    printf(" time (gpu)= %f mm.\n Calc %i elem\n", g_time, N * M);
     return c;
 }
 
@@ -98,3 +106,4 @@ DELLEXPORT int getCudaDev() {
     cudaGetDeviceCount(&cuda_count);
     return cuda_count;
 }
+
