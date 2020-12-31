4976a34ee33501a90a0f5725deca0d60d689da47
Cherepanov Igor <zigorewslike@gmail.com>
2020-12-31 22:08:32 +0800

    "Частичная" реализация свёртки к rgb в windows form

--- cuda_convolution_UI/Form1.Designer.cs
+++ cuda_convolution_UI/Form1.Designer.cs
@@ -39,9 +39,16 @@ namespace cuda_convolution_UI
             this.stFooterChild3 = new System.Windows.Forms.ToolStripStatusLabel();
             this.stFooterChild4 = new System.Windows.Forms.ToolStripStatusLabel();
             this.stFooterChild5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nudConvSize = new System.Windows.Forms.NumericUpDown();
+            this.lblform = new System.Windows.Forms.Label();
+            this.btnAply = new System.Windows.Forms.Button();
+            this.lblLoading = new System.Windows.Forms.Label();
             this.stFooter.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbResault)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudConvSize)).BeginInit();
             this.SuspendLayout();
             // 
             // stFooter
@@ -52,9 +59,10 @@ namespace cuda_convolution_UI
             this.stFooterChild3,
             this.stFooterChild4,
             this.stFooterChild5});
-            this.stFooter.Location = new System.Drawing.Point(0, 496);
+            this.stFooter.Location = new System.Drawing.Point(0, 757);
             this.stFooter.Name = "stFooter";
-            this.stFooter.Size = new System.Drawing.Size(954, 22);
+            this.stFooter.Size = new System.Drawing.Size(962, 22);
+            this.stFooter.SizingGrip = false;
             this.stFooter.TabIndex = 0;
             this.stFooter.Text = "statusStrip1";
             // 
@@ -71,7 +79,7 @@ namespace cuda_convolution_UI
             // pbOriginal
             // 
             this.pbOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbOriginal.Location = new System.Drawing.Point(2, 53);
+            this.pbOriginal.Location = new System.Drawing.Point(1, 35);
             this.pbOriginal.Name = "pbOriginal";
             this.pbOriginal.Size = new System.Drawing.Size(460, 422);
             this.pbOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -82,12 +90,13 @@ namespace cuda_convolution_UI
             // pbResault
             // 
             this.pbResault.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbResault.Location = new System.Drawing.Point(485, 53);
+            this.pbResault.Location = new System.Drawing.Point(502, 35);
             this.pbResault.Name = "pbResault";
             this.pbResault.Size = new System.Drawing.Size(460, 422);
             this.pbResault.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbResault.TabIndex = 3;
             this.pbResault.TabStop = false;
+            this.pbResault.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbResault_MouseMove);
             // 
             // lblImgWidth
             // 
@@ -101,49 +110,122 @@ namespace cuda_convolution_UI
             // stFooterChild1
             // 
             this.stFooterChild1.Name = "stFooterChild1";
-            this.stFooterChild1.Size = new System.Drawing.Size(118, 17);
-            this.stFooterChild1.Text = "toolStripStatusLabel1";
+            this.stFooterChild1.Size = new System.Drawing.Size(89, 17);
+            this.stFooterChild1.Text = "Cuda devices: 0";
             // 
             // stFooterChild2
             // 
             this.stFooterChild2.Name = "stFooterChild2";
-            this.stFooterChild2.Size = new System.Drawing.Size(118, 17);
-            this.stFooterChild2.Text = "toolStripStatusLabel1";
+            this.stFooterChild2.Size = new System.Drawing.Size(66, 17);
+            this.stFooterChild2.Text = "status: chill";
             // 
             // stFooterChild3
             // 
             this.stFooterChild3.Name = "stFooterChild3";
-            this.stFooterChild3.Size = new System.Drawing.Size(118, 17);
-            this.stFooterChild3.Text = "toolStripStatusLabel1";
+            this.stFooterChild3.Size = new System.Drawing.Size(88, 17);
+            this.stFooterChild3.Text = "time load: 0 ms";
             // 
             // stFooterChild4
             // 
             this.stFooterChild4.Name = "stFooterChild4";
-            this.stFooterChild4.Size = new System.Drawing.Size(118, 17);
-            this.stFooterChild4.Text = "toolStripStatusLabel1";
+            this.stFooterChild4.Size = new System.Drawing.Size(82, 17);
+            this.stFooterChild4.Text = "time edit 0 ms";
             // 
             // stFooterChild5
             // 
             this.stFooterChild5.Name = "stFooterChild5";
-            this.stFooterChild5.Size = new System.Drawing.Size(118, 17);
-            this.stFooterChild5.Text = "toolStripStatusLabel1";
+            this.stFooterChild5.Size = new System.Drawing.Size(74, 17);
+            this.stFooterChild5.Text = "mouse pos 0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.nudConvSize);
+            this.panel1.Controls.Add(this.lblform);
+            this.panel1.Location = new System.Drawing.Point(12, 463);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(637, 268);
+            this.panel1.TabIndex = 8;
+            // 
+            // nudConvSize
+            // 
+            this.nudConvSize.Location = new System.Drawing.Point(169, 3);
+            this.nudConvSize.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.nudConvSize.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudConvSize.Name = "nudConvSize";
+            this.nudConvSize.Size = new System.Drawing.Size(67, 20);
+            this.nudConvSize.TabIndex = 9;
+            this.nudConvSize.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudConvSize.ValueChanged += new System.EventHandler(this.nudConvSize_ValueChanged);
+            // 
+            // lblform
+            // 
+            this.lblform.AutoSize = true;
+            this.lblform.Location = new System.Drawing.Point(0, 5);
+            this.lblform.Name = "lblform";
+            this.lblform.Size = new System.Drawing.Size(167, 13);
+            this.lblform.TabIndex = 10;
+            this.lblform.Text = "Размерность матрицы свёртки";
+            // 
+            // btnAply
+            // 
+            this.btnAply.Enabled = false;
+            this.btnAply.Location = new System.Drawing.Point(463, 209);
+            this.btnAply.Name = "btnAply";
+            this.btnAply.Size = new System.Drawing.Size(37, 35);
+            this.btnAply.TabIndex = 11;
+            this.btnAply.Text = ">";
+            this.btnAply.UseVisualStyleBackColor = true;
+            this.btnAply.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.BackColor = System.Drawing.Color.White;
+            this.lblLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLoading.Location = new System.Drawing.Point(503, 199);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(459, 52);
+            this.lblLoading.TabIndex = 12;
+            this.lblLoading.Text = "ОБРАБОТКА ...";
+            this.lblLoading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLoading.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 518);
+            this.ClientSize = new System.Drawing.Size(962, 779);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.btnAply);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblImgWidth);
             this.Controls.Add(this.pbResault);
             this.Controls.Add(this.pbOriginal);
             this.Controls.Add(this.btnU);
             this.Controls.Add(this.stFooter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
             this.stFooter.ResumeLayout(false);
             this.stFooter.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbResault)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudConvSize)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +243,11 @@ namespace cuda_convolution_UI
         private System.Windows.Forms.ToolStripStatusLabel stFooterChild4;
         private System.Windows.Forms.ToolStripStatusLabel stFooterChild5;
         private System.Windows.Forms.Label lblImgWidth;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown nudConvSize;
+        private System.Windows.Forms.Label lblform;
+        private System.Windows.Forms.Button btnAply;
+        private System.Windows.Forms.Label lblLoading;
     }
 }
 
--- cuda_convolution_UI/Form1.cs
+++ cuda_convolution_UI/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
 
 namespace cuda_convolution_UI
 {
@@ -15,13 +18,35 @@ namespace cuda_convolution_UI
     {
         [DllImport("cudaFanc64.dll", CharSet = CharSet.Ansi, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
         public static extern int getCudaDev();
+        [DllImport("cudaFanc64.dll", CharSet = CharSet.Ansi, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr calcConvolutionCuda(int N, int M, IntPtr img, IntPtr conv, int cN);
+
+        private int image_width;
+        private int image_height;
+        private List<NumericUpDown> numud_list = new List<NumericUpDown>();
+        private const int default_cach = 128;
+        private int cache_size = default_cach;
         public Form1()
         {
             InitializeComponent();
-            stFooterChild1.Text = "Cuda devices: " + getCudaDev().ToString();
+            stFooterChild1.Text = "Cuda devices: " + getCudaDev().ToString() + " |";
             if (getCudaDev() < 1)
             {
                 MessageBox.Show("Not CUDA supporting", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    NumericUpDown numbUD = new NumericUpDown();
+                    numbUD.Top = i * 20 + 25;
+                    numbUD.Width = 45;
+                    numbUD.Minimum = -numbUD.Maximum;
+                    numbUD.Left = (numbUD.Width + 20) * j;
+                    numud_list.Add(numbUD);
+                    if (i == 1 && j == 1) numbUD.Value = 1;
+                    this.panel1.Controls.Add(numbUD);
+                }
             }
         }
 
@@ -42,9 +67,166 @@ namespace cuda_convolution_UI
             }
         }
 
+        public async void apply_conv_matrix(int[] conv_array, Bitmap img)
+        {
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
+            for (int chl = 0; chl < 3; chl++)
+            {
+                for (int new_height = cache_size; new_height < image_height + cache_size - 1; new_height += cache_size)
+                {
+                    //MessageBox.Show("H:" + new_height.ToString());
+                    if (new_height > image_height) new_height = image_height;
+                    for (int new_width = cache_size; new_width < image_width + cache_size - 1; new_width += cache_size)
+                    {
+                        if (new_width > image_width) new_width = image_width;
+                        //MessageBox.Show(new_width.ToString());
+
+                        int[] img_array = new int[cache_size * cache_size];
+
+                        for (int i = new_width - cache_size; i < new_width; i++)
+                        {
+                            for (int j = new_height - cache_size; j < new_height; j++)
+                            {
+                                if (chl == 0) img_array[1 * ((i % cache_size) * cache_size + (j % cache_size))] = img.GetPixel(i, j).R;
+                                else if (chl == 1) img_array[1 * ((i % cache_size) * cache_size + (j % cache_size))] = img.GetPixel(i, j).G;
+                                else img_array[1 * ((i % cache_size) * cache_size + (j % cache_size))] = img.GetPixel(i, j).B;
+                            }
+                        }
+                        GCHandle handle_image = GCHandle.Alloc(img_array, GCHandleType.Pinned);
+                        GCHandle handle_conv = GCHandle.Alloc(conv_array, GCHandleType.Pinned);
+                        try
+                        {
+                            IntPtr pointer_img = handle_image.AddrOfPinnedObject();
+                            IntPtr pointer_conv = handle_conv.AddrOfPinnedObject();
+                            //MessageBox.Show("Pointer is writed");
+
+                            IntPtr c = calcConvolutionCuda(cache_size, cache_size, pointer_img, pointer_conv, (int)Math.Sqrt(conv_array.Length));
+                            int[] res = new int[cache_size * cache_size];
+                            Marshal.Copy(c, res, 0, cache_size * cache_size);
+                            for (int i = (new_width - 1) / cache_size * cache_size; i < new_width; i++)
+                            {
+                                for (int j = (new_height - 1) / cache_size * cache_size; j < new_height; j++)
+                                {
+                                    int r = res[i % cache_size * cache_size + (j % cache_size)];
+                                    Color cl = img.GetPixel(i, j);
+                                    if (chl == 0) img.SetPixel(i, j, Color.FromArgb(r, cl.G, cl.B));
+                                    else if (chl == 1) img.SetPixel(i, j, Color.FromArgb(cl.R, r, cl.B));
+                                    else img.SetPixel(i, j, Color.FromArgb(cl.R, cl.G, r));
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            if (handle_image.IsAllocated)
+                            {
+                                handle_image.Free();
+                            }
+                            if (handle_conv.IsAllocated)
+                            {
+                                handle_conv.Free();
+                            }
+                        }
+                    }
+
+                }
+            }
+            stopWatch2.Stop();
+            TimeSpan ts2 = stopWatch2.Elapsed;
+            stFooterChild4.Text = "time edit img: " + (ts2.Seconds*1000 + ts2.Milliseconds).ToString() + "ms";
+            stFooterChild2.Text = "satus: img edit";
+            lblLoading.Visible = false;
+            btnAply.Enabled = true;
+            this.Update();
+        }
+
         private void pbOriginal_LoadCompleted(object sender, AsyncCompletedEventArgs e)
         {
+            btnAply.Enabled = true;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
             lblImgWidth.Text = "width: " + pbOriginal.Image.Width + " height: " + pbOriginal.Image.Height;
+            image_width = pbOriginal.Image.Width;
+            image_height = pbOriginal.Image.Height;
+            if (image_height < cache_size || image_width < cache_size)
+            {
+                cache_size = Math.Min(image_height, image_width);
+            }
+            else if (cache_size != default_cach) cache_size = default_cach;
+            stFooterChild2.Text = "satus: img load - OK";
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            stFooterChild3.Text = "time load img: " + (ts.Milliseconds).ToString() + "ms";
+            
+            //int[] conv_array = new int[9] { 1, 0, -1, 2, 0, -2, 1, 0, -1 };                                                     // Прикольный фильтер Собеля
+            //int[] conv_array = new int[9] { -2, -1, 0, -1, 1, 1, 0, 1, 2 };                                                     // Тиснение
+            //int[] conv_array = new int[25] { 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 };       // пацанское размытие
+            //int[] conv_array = new int[25] { 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, -1, 5, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0 };   // резкость
+            //int[] conv_array = new int[9] { 0, 1, 0, 1, -4, 1, 0, 1, 0 };                                                       // Хорошо выраженные края
+
+            Bitmap img = new Bitmap(pbOriginal.Image);
+            //apply_conv_matrix(conv_array, img);
+            pbResault.Image = img;
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            pbOriginal.Width = this.Width / 2 - 40;
+            pbOriginal.Height = this.Height - 120 - panel1.Height;
+            lblLoading.Left = pbResault.Left = this.Width / 2 + 20;
+            lblLoading.Width =  pbResault.Width = this.Width / 2 - 40;
+            pbResault.Height = this.Height - 120 - panel1.Height;
+            
+            btnAply.Left = this.Width / 2 - 27;
+            btnAply.Top = pbOriginal.Height / 2 + pbOriginal.Top - (btnAply.Height / 2);
+            panel1.Top = this.Height - panel1.Height - 70;
+            lblLoading.Top = pbOriginal.Height / 2 + pbOriginal.Top - (lblLoading.Height / 2);
+        }
+
+        private void pbResault_MouseMove(object sender, MouseEventArgs e)
+        {
+            float pos_x = (float)e.Location.X / (float)pbResault.Width * (float)image_width;
+            float pos_y = (float)e.Location.Y / (float)pbResault.Height * (float)image_height;
+            stFooterChild5.Text = "| mouse pos: " + ((int)pos_x).ToString() + ":" + ((int)pos_y).ToString();
+        }
+
+        private void nudConvSize_ValueChanged(object sender, EventArgs e)
+        {
+            int val = (int)nudConvSize.Value;
+            numud_list.ForEach(delegate (NumericUpDown numbUD) {
+                this.panel1.Controls.Remove(numbUD);
+            });
+            numud_list.Clear();
+
+            for (int i = 0; i < val; i++)
+            {
+                for (int j = 0; j < val; j++)
+                {
+                    NumericUpDown numbUD = new NumericUpDown();
+                    numbUD.Top = i * 20 + 25;
+                    numbUD.Width = 45;
+                    numbUD.Minimum = -numbUD.Maximum;
+                    numbUD.Left = (numbUD.Width + 20) * j;
+                    numud_list.Add(numbUD);
+                    this.panel1.Controls.Add(numbUD);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblLoading.Visible = true;
+            btnAply.Enabled = false;
+            this.Update();
+            int val = (int)nudConvSize.Value;
+            int[] conv_matrix = new int[val * val];
+            for (int i = 0; i < val * val; i++)
+            {
+                conv_matrix[i] = (int)numud_list.ElementAt(i).Value;
+            }
+            Bitmap img = new Bitmap(pbOriginal.Image);
+            apply_conv_matrix(conv_matrix, img);
+            pbResault.Image = img;
         }
     }
 }
